d1fc943140aadf280641e87c185a4b54a56dfcea
jeanettepayne <jeanetterpayne@gmail.com>
2021-05-04 21:25:11 -0400

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,13 @@ nursery = Board.create(title: "Nursery Vision")
 
 
 dress = Item.create(name: "Hayley Paige", board_id: 1)
+venue = Item.create(name: "Piedmont Room", board_id: 1)
+photog = Item.create(name: "BritRaePhoto", board_id: 1)
+
+job = Item.create(name: "Senior Developer", board_id: 2)
+salary = Item.create(name: "$$$", board_id: 2)
+style = Item.create(name: "Flexible/Balanced", 2)
+
+crib = Item.create(name: "Fancy Crib", board_id: 3)
+chair = Item.create(name: "Cool Glider", board_id: 3)
+snoo = Item.create(name: "Snoo", board_id: 3)
