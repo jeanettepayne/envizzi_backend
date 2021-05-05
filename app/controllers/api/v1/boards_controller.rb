ef0122544a141f72a876a457be9958078f3b8b7c
jeanettepayne <jeanetterpayne@gmail.com>
2021-05-04 22:19:56 -0400

    added index and create actions to boards controller

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::BoardsController < ApplicationController
+
+    def index
+        boards = Board.all
+        render json: boards
+    end
+
+    def create
+        board = Board.new(board_params)
+        if board.save
+            render json: board, status: :accepted
+        else
+            render json: {errors: board.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def board_params
+        params.require(:board).permit(:title)
+    end
 end
