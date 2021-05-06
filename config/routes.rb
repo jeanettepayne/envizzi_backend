d1f33d16637ae115d451afd6bc22b3ae98a40cee
jeanettepayne <jeanetterpayne@gmail.com>
2021-05-05 20:39:05 -0400

    added items index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :boards, only: [:index, :create]
+      resources :items, only: [:index]
     end
   end
 end
